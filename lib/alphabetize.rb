ee4e21c2c5b55256fcd32475e016c2f82f09b825
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-06-04 17:21:02 -0400

    Done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+# Gonna need to ask about this one
+# Got answer from stackoverflow
+# https://stackoverflow.com/questions/28368857/how-to-sort-an-array-of-accented-words-in-ruby
+
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |phrase|
+    phrase.chars.map do |char|
+      esperanto_alphabet.index(char)
+    end
+  end
 end
